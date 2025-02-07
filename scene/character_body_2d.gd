5ea0fdd0b92b9ce700e1ef803f4cea5e85510296
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-02-07 20:38:55 +0530

    idle imp

--- scene/character_body_2d.gd
+++ scene/character_body_2d.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-
-
+@onready
+var sprite = $AnimatedSprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -350.0
 
 
 func _physics_process(delta: float) -> void:
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+		sprite.play("idle")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
