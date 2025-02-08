786d76b9e4abf7c9f1fda419056dbb10a1873d2f
xXNannu09Xx <ngtty2020@gmail.com>
2025-02-08 15:05:54 +0530

    Basic Animation
    want someone to change ui_ to actions

--- script/player_script.gd
+++ script/player_script.gd
@@ -6,6 +6,8 @@ var sprite = $AnimatedSprite2D
 const SPEED = 275.0
 const JUMP_VELOCITY = -350.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,7 +19,23 @@ func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	#get input direction
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	#Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	#Play animation
+	if direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("walk")
+	
+	#Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
