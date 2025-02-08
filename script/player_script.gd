67ccfa769406660c866347f30abbba81d37636d2
xXNannu09Xx <ngtty2020@gmail.com>
2025-02-08 14:57:10 +0530

    organizing stuff

--- script/player_script.gd
+++ script/player_script.gd
@@ -12,14 +12,9 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	if Input.is_key_pressed(KEY_RIGHT):
-		sprite.play("walk")
-	else:
-		sprite.play("idle")	
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
