a213fc7bbaf524f2ed4d3ccffb80bd45d0c05d5a
xXNannu09Xx <ngtty2020@gmail.com>
2025-02-09 14:46:03 +0530

    Portals!

--- script/level_2.gd
+++ script/level_2.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 
+func on_level_portal_2_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://scene/level_3.tscn")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://scene/level_3.tscn") # Replace with function body.
