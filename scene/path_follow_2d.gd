2bc253c585d4596e01909edef95cf87e4fdb71c3
xXNannu09Xx <ngtty2020@gmail.com>
2025-02-25 01:01:53 +0530

    moving platform v1

--- scene/path_follow_2d.gd
+++ scene/path_follow_2d.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	progress_ratio = delta * 0.2
+	progress_ratio = delta * 2
