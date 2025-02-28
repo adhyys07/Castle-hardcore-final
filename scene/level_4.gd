ff4cc6660cba6d066430e6fdfdab2878c6ce70f4
xXNannu09Xx <ngtty2020@gmail.com>
2025-02-28 22:33:24 +0530

    updated level 4

--- scene/level_4.gd
+++ scene/level_4.gd
@@ -9,7 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scene/level_4.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
