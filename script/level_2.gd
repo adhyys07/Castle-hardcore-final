4aa2a7d7ef87c30261bf9d2443fe8225f7d87808
xXNannu09Xx <ngtty2020@gmail.com>
2025-03-01 21:21:46 +0530

    debug

--- script/level_2.gd
+++ script/level_2.gd
@@ -1,20 +1,6 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
-# Called when the node enters the scene tree for the first time.
-
-func on_level_portal_2_entered(body: Node2D) -> void:
+func _on_level_portal_2_body_entered(body: Player):
 	get_tree().change_scene_to_file("res://scene/level_3.tscn")
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scene/level_3.tscn") # Replace with function body.print(label)
