30ee2cdca09ff3b4606aa72a844f04c07245de67
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-03-04 15:57:47 +0530

    updated movin mob

--- script/movin_mob.gd
+++ script/movin_mob.gd
@@ -1,11 +1,25 @@
 extends Path2D
 
+@onready var sprite = $AnimatedSprite2D 
+@export var loop = false
+@export var speed = 2.0
+@export var speed_scale = 2.0
+@onready var path = $PathFollow2D
+@onready var animation = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	if not loop:
+		animation.play("move")
+		animation.speed_scale = speed_scale
+		set_process(false)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	path.progress+=speed
+ # Replace with function body.
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if (body.name == "Player"):
+		print("You died! bitch") # Replace with function body.
+		body.queue_free()
+		get_tree().change_scene_to_file("res://scene/try_again.tscn")
