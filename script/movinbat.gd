c506665bc8302235a2c63dc04dcd95d3622feccf
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-03-06 15:48:36 +0530

    fixed raycast

--- script/movinbat.gd
+++ script/movinbat.gd
@@ -1,39 +1,34 @@
 extends CharacterBody2D
-const SPEED = 50
-@onready var sprite = $AnimatedSprite2D
-var direction = 1
-var inverse = -1
+
+@export var speed: float = 100  # Movement speed
+@onready var ray_cast = $RayCast2D  # Downward RayCast2D
+
+var initial_direction: int = 1  # Default direction (right)
+var direction: int  # This will be set in _ready()
 
 func _ready():
-	pass
-	
-	
-func enemy_gravity(delta : float):
-	if not is_on_floor():
-		velocity += delta * get_gravity()
+	# Reset direction when the scene is reloaded
+	direction = initial_direction
+	scale.x = 1 if direction == 1 else -1  # Ensure correct sprite orientation
+	ray_cast.position.x = abs(ray_cast.position.x) * direction  # Reset RayCast2D position
 
-func rayc():
-	if not $RayCast2D.is_colliding():
-		direction = -direction
-		$RayCast2D.position.x *= -1
-
-func move_enemy():
-	velocity.x = SPEED * direction 
-
-func reverse_dir():
-	if is_on_wall():
-		direction = -direction
-		
-	
-func _physics_process(delta: float) -> void:
-	enemy_gravity(delta)
-	move_enemy()
+func _physics_process(delta):
+	velocity.x = direction * speed
 	move_and_slide()
-	reverse_dir()
-	rayc()
+
+	# If no ground ahead OR if the mob hits a wall, flip direction
+	if not ray_cast.is_colliding() or is_on_wall():
+		flip_direction()
+
+func flip_direction():
+	direction *= -1
+	scale.x *= -1
+	ray_cast.position.x *= -1
+	position.x += direction * 5  # Push slightly to prevent getting stuck
 
 
-func _on_area_2d_body_entered(body: Player):
-	print("You died! bitch") # Replace with function body.
-	body.queue_free()
-	get_tree().change_scene_to_file("res://scene/global/try_again.tscn")
+func _on_area_2d_body_entered(body: Node2D):
+	if (body.name == "Player"):
+		print("You died! bitch") # Replace with function body.
+		body.queue_free()
+		get_tree().change_scene_to_file("res://scene/global/try_again.tscn")
