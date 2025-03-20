3bd538f6f34892c734d8e2d7adc20c75da854a7e
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-03-20 15:03:08 +0530

    update

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -18,3 +18,7 @@ func _on_button_pressed() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
--- script/shop.gd
+++ script/shop.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var new_player = preload("res://scene/playerskins/player_gsword.tscn").instantiate()
 
 # popup.gd
 func _ready():
@@ -8,3 +9,17 @@ func _ready():
 
 func _on_close_pressed():
 	queue_free()
+
+
+func _on_gsword_pressed() -> void:
+	get_parent().add_child(new_player)
+	queue_free() # Remove the current player scene
+
+
+
+func _on_ghammer_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	visible = false # Replace with function body.
